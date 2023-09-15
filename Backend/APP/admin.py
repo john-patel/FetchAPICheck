c44f6b52981e9ea62fb424c1d46f1b3dcb5588cd
Parth Patel <58204750+john-patel@users.noreply.github.com>
2023-09-15 23:08:28 +0530

    Delete Backend directory

--- Backend/APP/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models  import getData
-
-admin.site.register(getData)
--- Backend/Backend/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from APP import views as vw
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', vw.SendData)
-]
